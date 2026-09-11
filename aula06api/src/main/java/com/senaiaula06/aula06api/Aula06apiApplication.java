cd09843f8128ed6614083bff147d00fecd731083
rechereaphael-Re <recheraphael@gmail.com>
2026-09-10 22:11:24 -0300

    feature_aula06_springboot

---
+++ aula06api/src/main/java/com/senaiaula06/aula06api/Aula06apiApplication.java
@@ -0,0 +1,13 @@
+package com.senaiaula06.aula06api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula06apiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula06apiApplication.class, args);
+	}
+
+}
