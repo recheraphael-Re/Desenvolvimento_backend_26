f68353b387bde8dc7f5c25ebb50705caea4f0ad6
rechereaphael-Re <recheraphael@gmail.com>
2026-09-21 19:45:04 -0300

    featura_inicio

---
+++ aula08cti/cit-senai/src/main/java/com/senai/cit_senai/CitSenaiApplication.java
@@ -0,0 +1,13 @@
+package com.senai.cit_senai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitSenaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitSenaiApplication.class, args);
+	}
+
+}
---
+++ aula08cti/cit-senai/src/test/java/com/senai/cit_senai/CitSenaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.cit_senai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitSenaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
